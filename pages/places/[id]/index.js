ad44ce7480d5f1c1dbd4e3f706a2026531caaaa2
CtzarGM <cesarga4649@google.com>
2023-06-12 14:16:29 +0200

    fix .env file to connect to the cloud - replace place variable with get method at api/places/index.js & api/places/[id]/index.js - change id for _id at places/[id]

--- pages/places/[id]/index.js
+++ pages/places/[id]/index.js
@@ -33,7 +33,7 @@ export default function DetailsPage() {
   const { isReady } = router;
   const { id } = router.query;
 
-  const { data: place, isLoading, error } = useSWR(`/api/places/${id}`);
+  const { data: place, isLoading, error } = useSWR(`/api/places/${_id}`);
 
   if (!isReady || isLoading || error) return <h2>Loading...</h2>;
 
